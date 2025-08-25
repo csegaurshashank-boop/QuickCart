22cce9163a943a49b068174b49507e0734555f2d
Shashank Gaur <csegarshashank@gmail.com>
2025-08-25 13:28:29 +0530

    fix: updated inngest config and user model

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,6 +1,5 @@
 import { serve } from "inngest/next";
-import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
-
+import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "../../../config/inngest";
 
 // Create an API that serves your functions
 export const { GET, POST, PUT } = serve({
