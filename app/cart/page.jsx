f1286f947c3d68bb14ab6a9ac36a421afb2b55c2
Shashank Gaur <csegarshashank@gmail.com>
2025-08-31 13:41:31 +0530

    Remove .env file from repo

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -75,7 +75,7 @@ const Cart = () => {
                           </button>
                         </div>
                       </td>
-                      <td className="py-4 md:px-4 px-1 text-gray-600">₹{product.offerPrice}</td>
+                      <td className="py-4 md:px-4 px-1 text-gray-600">${product.offerPrice}</td>
                       <td className="py-4 md:px-4 px-1">
                         <div className="flex items-center md:gap-2 gap-1">
                           <button onClick={() => updateCartQuantity(product._id, cartItems[itemId] - 1)}>
