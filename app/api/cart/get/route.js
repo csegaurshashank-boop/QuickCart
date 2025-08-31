e13d8208016310c170896010d2d94bb90c1d52bd
Shashank Gaur <csegarshashank@gmail.com>
2025-08-31 12:38:48 +0530

    Remove .env file from repo

--- app/api/cart/get/route.js
+++ app/api/cart/get/route.js
@@ -1,23 +1,19 @@
 import connectDB from "@/config/db";
 import User from "@/models/User";
-import { getAuth } from "@clerk/nextjs/dist/types/server";
+import { getAuth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-
 export async function GET(request) {
+  try {
+    const { userId } = getAuth(request);
 
-    try {
-        const { userId } = getAuth(request)
+    await connectDB();
+    const user = await User.findById(userId);
 
-        await connectDB()
-        const user = await User.findById(userId)
+    const { cartItems } = user;
 
-        const { cartItems } = user
-
-        return NextResponse.json({ success: true, cartItems })
-
-    } catch (error) {
-        return NextResponse.json({success: false, message: error.message})
-    }
-    
+    return NextResponse.json({ success: true, cartItems });
+  } catch (error) {
+    return NextResponse.json({ success: false, message: error.message });
+  }
 }
